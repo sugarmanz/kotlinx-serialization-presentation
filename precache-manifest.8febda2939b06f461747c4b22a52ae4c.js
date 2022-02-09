662262a2a0799d64bf5a68262ef0d72a6a164daf
sugarmanz <zucker.jeremiah@gmail.com>
2022-02-09 01:30:31 +0000

    Deploying to gh-pages from @ sugarmanz/kotlinx-serialization-presentation@92991eb5709e10d7dd1c4d0fcaddf89d15a09416 🚀

---
+++ precache-manifest.8febda2939b06f461747c4b22a52ae4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f89aacfca48e14ad6c6cb5d74555a12",
+    "url": "/kotlinx-serialization-presentation/index.html"
+  },
+  {
+    "revision": "b000cf1e986e1b3f2fc8",
+    "url": "/kotlinx-serialization-presentation/static/css/main.3451aed5.chunk.css"
+  },
+  {
+    "revision": "f5b8f3e9691ae7ceb603",
+    "url": "/kotlinx-serialization-presentation/static/js/2.871923ef.chunk.js"
+  },
+  {
+    "revision": "f74a6debb7da89320918198b818026f8",
+    "url": "/kotlinx-serialization-presentation/static/js/2.871923ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b000cf1e986e1b3f2fc8",
+    "url": "/kotlinx-serialization-presentation/static/js/main.ddc8eff8.chunk.js"
+  },
+  {
+    "revision": "04328526c5eed5684626",
+    "url": "/kotlinx-serialization-presentation/static/js/runtime-main.7f54d9d1.js"
+  }
+]);
